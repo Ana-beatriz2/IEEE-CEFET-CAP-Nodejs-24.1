670f1a31647158faf58e2046df7ac068e50c2bf8
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-05-01 19:14:53 -0300

    feat: adiciona informação da porta e do nome da database no dotenv

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 module.exports = {
     client: "mysql2", //banco que estou usando
     connection: {
-        database: "apicap", // create database apicap;
+        database: process.env.DB_DATABASE, // create database apicap;
 		user: process.env.DB_USER, 
-		password: process.env.DB_PASS,
-		port: 3306
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT
     },
     migrations: {
         tableName: "migrations", // nome da tabela que será criada no bd
